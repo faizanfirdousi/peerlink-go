0d0a7c3899bf4f74fbdc1f0941c14f7e4b31f881
faizanfirdousi <faizanfirdousi.official@gmail.com>
2025-07-30 15:48:56 +0530

    hmmmOC

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,14 +1,10 @@
 package p2p
 
 // Peer is an interface that represents the remote node
-type Peer interface {
-
-}
+type Peer interface {}
 
 
 // tansport is anything that handles the communication
-// between the nodes in the network
-type Transport interface {
-
-
-}
+// between the nodes in the network, this can be of the
+// form (TCP,UDP, websockets,... )
+type Transport interface {}
